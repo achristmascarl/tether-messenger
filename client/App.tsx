cf7ffbc182a66c87f6f6e0b2aabc09a7a73600b2
achristmascarl <carl.z.liu@gmail.com>
2022-09-01 17:55:34 -0400

    client and server folders

--- client/App.tsx
+++ client/App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App to start working on your app!</Text>
     </View>
   );
 }
@@ -15,4 +15,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); 
